b75a9b6eb2f50bc2fd614bef869d82dfda530707
Abhay Kumar Srivastava <ms2364911@gmail.com>
2023-11-28 18:22:05 +0530

    practice time

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = express();
+
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cookieParser());
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .cookie("token", "de3ifbhcvewij", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    })
+    .json({ message: "write success" });
+});
+
+app.get("/token", (req, res) => {
+  const token = req.cookies.token;
+
+  res.status(200).json({ token });
+});
+
+app.listen(4000, () => {
+  console.log("server started");
+});
